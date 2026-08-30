8a5177bf1e4624aa1a18fbc2e1d7e92412cc52ce
Owais Mahros <Owais.Mahros2@gmial.com>
2026-08-30 23:19:25 +0300

    solving last problem

--- src/Pages/Cart and payments/CartAndPayments.jsx
+++ src/Pages/Cart and payments/CartAndPayments.jsx
@@ -1,9 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-=======
-import './CartAndPayments.css';
-import { Navigate } from "react-router-dom";
->>>>>>> 0798639 (update styles and inherit from index)
 import ShoppingCart from "./pages/ShoppingCart.jsx"
 import CheckoutPage from "./pages/CheckoutPage.jsx";
 import OrderConfirmation from "./pages/OrderConfirmation.jsx";
